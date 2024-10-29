3fe0b5fc6e7f568f678fb1b39369cc5604c642e5
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-10-30 00:07:08 +0400

    used Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import StatusBarMessage from "./components/UI/StatusBarMessage";
-import { CartRequestSliceActions } from "./components/store/CartRequestSlice";
+import { sendCartData } from "./components/store/CartRequestSlice";
+// import { CartRequestSliceActions } from "./components/store/CartRequestSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 let isAppRunningFirst = true;
-let deleteStatusBar;
-let isStatusBarDeleting = false;
+// let deleteStatusBar;
+// let isStatusBarDeleting = false;
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -18,54 +19,54 @@ function App() {
   const dispatchFunction = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-      try {
-        dispatchFunction(
-          CartRequestSliceActions.showMessageRequest({
-            requestStatus: "pending",
-            requestTitle: "Отправка данных.",
-            requestMessage: "Данные корзины отправляются на сервер...",
-          })
-        );
-        const response = await fetch(
-          "https://sushiapp-3c0b7-default-rtdb.firebaseio.com/cart.json",
-          {
-            method: "PUT",
-            body: JSON.stringify(cartState),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        if (!response.ok) {
-          throw new Error("Ошибка при отправке данных корзины");
-        }
-        dispatchFunction(
-          CartRequestSliceActions.showMessageRequest({
-            requestStatus: "success",
-            requestTitle: "Успешно!",
-            requestMessage: "Товар был успешно добавлен в корзину!",
-          })
-        );
-        if (isStatusBarDeleting) {
-          clearTimeout(deleteStatusBar);
-        }
-        deleteStatusBar = setTimeout(() => {
-          dispatchFunction(CartRequestSliceActions.deleteMessageRequest());
-          isStatusBarDeleting = true;
-        }, 1500);
-      } catch (err) {
-        dispatchFunction(
-          CartRequestSliceActions.showMessageRequest({
-            requestStatus: "error",
-            requestTitle: "Неудача!",
-            requestMessage: "Произошла ошибка при добавлении товара в корзину!",
-          })
-        );
-        console.error(`Произошла ошибка: ${err.message}`);
-      }
-    };
-    if (!isAppRunningFirst) sendCartData();
+    // const sendCartData = async () => {
+    //   try {
+    //     dispatchFunction(
+    //       CartRequestSliceActions.showMessageRequest({
+    //         requestStatus: "pending",
+    //         requestTitle: "Отправка данных.",
+    //         requestMessage: "Данные корзины отправляются на сервер...",
+    //       })
+    //     );
+    //     const response = await fetch(
+    //       "https://sushiapp-3c0b7-default-rtdb.firebaseio.com/cart.json",
+    //       {
+    //         method: "PUT",
+    //         body: JSON.stringify(cartState),
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //       }
+    //     );
+    //     if (!response.ok) {
+    //       throw new Error("Ошибка при отправке данных корзины");
+    //     }
+    //     dispatchFunction(
+    //       CartRequestSliceActions.showMessageRequest({
+    //         requestStatus: "success",
+    //         requestTitle: "Успешно!",
+    //         requestMessage: "Товар был успешно добавлен в корзину!",
+    //       })
+    //     );
+    //     if (isStatusBarDeleting) {
+    //       clearTimeout(deleteStatusBar);
+    //     }
+    //     deleteStatusBar = setTimeout(() => {
+    //       dispatchFunction(CartRequestSliceActions.deleteMessageRequest());
+    //       isStatusBarDeleting = true;
+    //     }, 1500);
+    //   } catch (err) {
+    //     dispatchFunction(
+    //       CartRequestSliceActions.showMessageRequest({
+    //         requestStatus: "error",
+    //         requestTitle: "Неудача!",
+    //         requestMessage: "Произошла ошибка при добавлении товара в корзину!",
+    //       })
+    //     );
+    //     console.error(`Произошла ошибка: ${err.message}`);
+    //   }
+    // };
+    if (!isAppRunningFirst) dispatchFunction(sendCartData(cartState));
     isAppRunningFirst = false;
   }, [cartState, dispatchFunction]);
 
--- src/components/store/CartRequestSlice.js
+++ src/components/store/CartRequestSlice.js
@@ -26,5 +26,62 @@ const CartRequestSlice = createSlice({
   },
 });
 
+let deleteStatusBar;
+let isStatusBarDeleting = false;
+
+export const sendCartData = (cartData) => {
+  return async (dispatchAction) => {
+    dispatchAction(
+      CartRequestSliceActions.showMessageRequest({
+        requestStatus: "pending",
+        requestTitle: "Отправка данных.",
+        requestMessage: "Данные корзины отправляются на сервер...",
+      })
+    );
+    const sendHttpRequest = async () => {
+      const response = await fetch(
+        "https://sushiapp-3c0b7-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Ошибка при отправке данных корзины");
+      }
+    };
+
+    try {
+      await sendHttpRequest();
+      dispatchAction(
+        CartRequestSliceActions.showMessageRequest({
+          requestStatus: "success",
+          requestTitle: "Успешно!",
+          requestMessage: "Товар был успешно добавлен в корзину!",
+        })
+      );
+      if (isStatusBarDeleting) {
+        clearTimeout(deleteStatusBar);
+      }
+      deleteStatusBar = setTimeout(() => {
+        dispatchAction(CartRequestSliceActions.deleteMessageRequest());
+        isStatusBarDeleting = true;
+      }, 1500);
+    } catch (err) {
+      dispatchAction(
+        CartRequestSliceActions.showMessageRequest({
+          requestStatus: "error",
+          requestTitle: "Неудача!",
+          requestMessage: "Произошла ошибка при добавлении товара в корзину!",
+        })
+      );
+      console.error(`Произошла ошибка: ${err.message}`);
+    }
+  };
+};
+
 export const CartRequestSliceActions = CartRequestSlice.actions;
 export default CartRequestSlice.reducer;
